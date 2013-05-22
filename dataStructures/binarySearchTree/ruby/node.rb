ab9023b1a9793b142c4735fc7916dd711cdefdb1
Danny Dyla <dyladan@gmail.com>
2013-05-22 16:58:20 -0400

    improved BST

--- dataStructures/binarySearchTree/ruby/node.rb
+++ dataStructures/binarySearchTree/ruby/node.rb
@@ -1,4 +1,4 @@
-class BinarySearchTree < Struct.new(:parent, :left, :right, :data)
+class BinarySearchTree < Struct.new(:parent, :left, :right, :key, :data)
   def id
     return @id
   end
@@ -8,50 +8,93 @@ class BinarySearchTree < Struct.new(:parent, :left, :right, :data)
 
   def to_h
     if self.left != nil && self.right != nil
-      return { :id => self.object_id, :parent => self.parent, :left => self.left.to_h, :right => self.right.to_h, :data => self.data }
+      return { :id => self.object_id, :parent => self.parent, :left => self.left.to_h, :right => self.right.to_h, :key => self.key, :data => self.data }
     elsif self.left == nil && self.right == nil
-      return { :id => self.object_id, :parent => self.parent, :left => nil, :right => nil, :data => self.data }
+      return { :id => self.object_id, :parent => self.parent, :left => nil, :right => nil, :key => self.key, :data => self.data }
     elsif self.left == nil
-      return { :id => self.object_id, :parent => self.parent, :left => nil, :right => self.right.to_h, :data => self.data }
+      return { :id => self.object_id, :parent => self.parent, :left => nil, :right => self.right.to_h, :key => self.key, :data => self.data }
     else
-      return { :id => self.object_id, :parent => self.parent, :left => self.left.to_h, :right => nil, :data => self.data }
+      return { :id => self.object_id, :parent => self.parent, :left => self.left.to_h, :right => nil, :key => self.key, :data => self.data }
     end
   end
 
-  def add x
-    if self.data == nil
-      self.data = x
+  def add x, y
+    if self.key == nil
+      self.key = x
+      self.data = y
       return
     end
-    if x < self.data
+    if self.key == x
+      self.data = y
+      return
+    end
+    if x < self.key
       if self.left == nil
-        self.left = BinarySearchTree.new(self.object_id, nil, nil, x)
+        self.left = BinarySearchTree.new(self.object_id, nil, nil, x, y)
       else
-        self.left.add(x)
+        self.left.add(x, y)
       end
-    elsif x > self.data
+    elsif x > self.key
       if self.right == nil
-        self.right = BinarySearchTree.new(self.object_id, nil, nil, x)
+        self.right = BinarySearchTree.new(self.object_id, nil, nil, x, y)
       else
-        self.right.add(x)
+        self.right.add(x, y)
       end
     end
+  end
+
+  def exists? key
+    if self.key == nil
+      return
+    end
+    if self.key == key
+      return true
+    end
+    if self.left != nil && key < self.key
+      return self.left.exists?(key)
+    elsif self.right != nil
+      return self.right.exists?(key)
+    end
+    if self.right == nil && self.left == nil
+      return false
+    end
+  end
+
+  def find key
+    if self.key == nil
+      return
+    end
+    if self.key == key
+      return self
+    end
+    if self.left != nil && key < self.key
+      return self.left.find(key)
+    elsif self.right != nil
+      return self.right.find(key)
+    end
+
   end
 
 end
-
-require 'json'
 require 'yaml'
+start = Time.now
 
-tree = BinarySearchTree.new()
+#tree = BinarySearchTree.new()
+hash = {}
 
-tree.add 8
-tree.add 9
-tree.add 1
-tree.add 100
+(0..100000).each do |x|
+  random = rand(100000)
+#  tree.add(random, "asdfasdf")
+  hash.store(random, "asdfasdf" )
+end
 
-json = tree.to_h.to_json
-yaml = tree.to_h.to_yaml
+middle = Time.now
 
-print json.to_s
-print yaml.to_s
+#print tree.find(100).data.to_s + "\n"
+print hash.fetch(100).to_s + "\n"
+
+stop = Time.now
+
+print "Time to populate tree: #{middle - start}\n"
+print "Time to search tree: #{stop - middle}\n"
+print "Total time: #{stop - start}\n"
