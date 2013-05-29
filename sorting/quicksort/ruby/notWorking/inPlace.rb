0a00d80f2164613a4ffa53fcd4bfb9bbbaa55581
Danny Dyla <dyladan@gmail.com>
2013-05-29 14:47:13 -0400

    moved inplace quicksort to show it isn't working

--- sorting/quicksort/ruby/notWorking/inPlace.rb
+++ sorting/quicksort/ruby/notWorking/inPlace.rb
@@ -16,3 +16,15 @@ def partition(array, left, right, pivotIndex)
   array[right] = temp
   return storeIndex
 end
+
+def quicksort(array, left, right)i
+  if left < right
+    pivotIndex = rand(left..right)
+    pivotNewIndex = partition(array, left, right, pivotIndex)
+    quicksort(array, left, pivotNewIndex - 1)
+    quicksort(array, pivotNewIndex + 1, right)
+  end
+end
+
+array = [1,5,3,5,4,2,6,1,5,5]
+quicksort(array, 0, 9)
